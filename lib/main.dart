086d0144d11a51cded09ece2afe4243fc4b9487b
anjumArnab <anjumarnab050@gmail.com>
2025-02-18 00:11:59 +0600

    google font added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:pseudo_json/screens/homepage.dart';
 
 void main() {
@@ -12,6 +13,9 @@ class PseudoJSON extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.latoTextTheme()
+      ),
       home:HomePage(),
     );
   }
