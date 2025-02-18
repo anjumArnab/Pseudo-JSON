2dcfc510e28abed3f4c6c685585ae059112b66fb
anjumArnab <anjumarnab050@gmail.com>
2025-02-18 13:05:49 +0600

    login functionality added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,18 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:pseudo_json/models/auth_user.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+class HomePage extends StatelessWidget {
+  // Accept AuthUser as a parameter
+  final AuthUser user;
 
-  @override
-  State<HomePage> createState() => _HomepageState();
-}
+  const HomePage({super.key, required this.user});
 
-class _HomepageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("P S E U D O  J S O N"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Welcome, ${user.firstName} ${user.lastName}!',
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 20),
+              Text('Email: ${user.email}'),
+              Text('Username: ${user.username}'),
+              Text('Role: ${user.role}'),
+              const SizedBox(height: 20),
+              user.image.isNotEmpty
+                  ? Image.network(user.image)
+                  : const Text('No image available'),
+            ],
+          ),
+        ),
       ),
     );
   }
