8dc59b42a2ce2a3b5cc6c33648747d9b92db665e
anjumArnab <anjumarnab050@gmail.com>
2025-02-20 00:01:20 +0600

    search and sort functionality added

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -11,11 +11,16 @@ class HomePage extends StatefulWidget {
   const HomePage({super.key, required this.user});
 
   @override
+  // ignore: library_private_types_in_public_api
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
   late Future<List<Product>?> _productsFuture;
+  bool isSearching = false;
+  TextEditingController searchController = TextEditingController();
+  String _selectedSortBy = 'price';
+  String _selectedOrder = 'asc';
 
   @override
   void initState() {
@@ -28,33 +33,135 @@ class _HomePageState extends State<HomePage> {
       final products = await getProductInfo();
       return products;
     } catch (e) {
-      print('Error fetching products: $e');
       return null;
     }
+  }
+
+  Future<void> searchProducts(String keyword) async {
+    setState(() {
+      _productsFuture = getSearchProduct(keyword);
+    });
+  }
+
+  // Update sorting and refresh products
+  void updateSorting(String sortBy, String order) {
+    setState(() {
+      _selectedSortBy = sortBy;
+      _selectedOrder = order;
+      _productsFuture = getSortProducts(_selectedSortBy, _selectedOrder);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Welcome ${widget.user.firstName}"),
+        title: isSearching
+            ? TextField(
+                controller: searchController,
+                autofocus: true,
+                decoration: const InputDecoration(
+                  hintText: 'Search by title...',
+                  hintStyle: TextStyle(color: Colors.black),
+                ),
+                onChanged: (value) {
+                  if (value.isNotEmpty) {
+                    searchProducts(value);
+                  } else {
+                    setState(() {
+                      _productsFuture = fetchProductInfo();
+                    });
+                  }
+                },
+              )
+            : Text("Welcome ${widget.user.firstName}"),
+        actions: [
+          IconButton(
+            icon: Icon(isSearching ? Icons.close : Icons.search),
+            onPressed: () {
+              setState(() {
+                isSearching = !isSearching;
+                if (!isSearching) {
+                  searchController.clear();
+                  _productsFuture = fetchProductInfo();
+                }
+              });
+            },
+          ),
+          DropdownButton<String>(
+            value: _selectedSortBy,
+            icon: const Icon(Icons.sort, color: Colors.black),
+            dropdownColor: Colors.white,
+            onChanged: (String? newValue) {
+              if (newValue != null) {
+                updateSorting(newValue, _selectedOrder);
+              }
+            },
+            items: const [
+              DropdownMenuItem(
+                value: 'price',
+                child: Text(
+                  'Price',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              DropdownMenuItem(
+                value: 'rating',
+                child: Text(
+                  'Rating',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              DropdownMenuItem(
+                value: 'discountPercentage',
+                child: Text(
+                  'Discount',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+          DropdownButton<String>(
+            value: _selectedOrder,
+            icon: const Icon(Icons.arrow_downward, color: Colors.black),
+            dropdownColor: Colors.white,
+            onChanged: (String? newValue) {
+              if (newValue != null) {
+                updateSorting(_selectedSortBy, newValue);
+              }
+            },
+            items: const [
+              DropdownMenuItem(
+                value: 'asc',
+                child: Text(
+                  'Ascending',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+              DropdownMenuItem(
+                value: 'desc',
+                child: Text(
+                  'Descending',
+                  style: TextStyle(color: Colors.black),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
       drawer: CustomDrawer(user: widget.user),
       body: FutureBuilder<List<Product>?>(
         future: _productsFuture,
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(child: CircularProgressIndicator());
+            return const Center(child: CircularProgressIndicator());
           }
-
           if (snapshot.hasError) {
             return Center(child: Text('Error: ${snapshot.error}'));
           }
-
           if (snapshot.data == null || snapshot.data!.isEmpty) {
-            return Center(child: Text('No products found'));
+            return const Center(child: Text('No products found'));
           }
-
           return Padding(
             padding: const EdgeInsets.all(8.0),
             child: GridView.builder(
