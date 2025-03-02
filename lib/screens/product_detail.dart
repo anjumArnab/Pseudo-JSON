67e5276222e9055a9fdc8d7ac563cb9e23b25124
anjumArnab <anjumarnab050@gmail.com>
2025-03-02 13:27:39 +0600

    navigaton changed

--- lib/screens/product_detail.dart
+++ lib/screens/product_detail.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:pseudo_json/models/products.dart';
 import 'package:pseudo_json/screens/product_detail_page.dart';
 import 'package:pseudo_json/services/api_services.dart';
-import 'package:pseudo_json/widgets/custom_button.dart';
 import 'package:pseudo_json/widgets/custom_text_field.dart';
 
 class ProductDetail extends StatefulWidget {
@@ -36,20 +35,31 @@ class _ProductDetailState extends State<ProductDetail> {
   void initState() {
     super.initState();
     titleController = TextEditingController(text: widget.product.title);
-    descriptionController = TextEditingController(text: widget.product.description);
+    descriptionController =
+        TextEditingController(text: widget.product.description);
     categoryController = TextEditingController(text: widget.product.category);
-    priceController = TextEditingController(text: widget.product.price.toString());
-    discountPercentageController = TextEditingController(text: widget.product.discountPercentage.toString());
-    ratingController = TextEditingController(text: widget.product.rating.toString());
-    stockController = TextEditingController(text: widget.product.stock.toString());
+    priceController =
+        TextEditingController(text: widget.product.price.toString());
+    discountPercentageController = TextEditingController(
+        text: widget.product.discountPercentage.toString());
+    ratingController =
+        TextEditingController(text: widget.product.rating.toString());
+    stockController =
+        TextEditingController(text: widget.product.stock.toString());
     brandController = TextEditingController(text: widget.product.brand);
     skuController = TextEditingController(text: widget.product.sku);
-    weightController = TextEditingController(text: widget.product.weight.toString());
-    warrantyInformationController = TextEditingController(text: widget.product.warrantyInformation);
-    shippingInformationController = TextEditingController(text: widget.product.shippingInformation);
-    availabilityStatusController = TextEditingController(text: widget.product.availabilityStatus);
-    returnPolicyController = TextEditingController(text: widget.product.returnPolicy);
-    minimumOrderQuantityController = TextEditingController(text: widget.product.minimumOrderQuantity.toString());
+    weightController =
+        TextEditingController(text: widget.product.weight.toString());
+    warrantyInformationController =
+        TextEditingController(text: widget.product.warrantyInformation);
+    shippingInformationController =
+        TextEditingController(text: widget.product.shippingInformation);
+    availabilityStatusController =
+        TextEditingController(text: widget.product.availabilityStatus);
+    returnPolicyController =
+        TextEditingController(text: widget.product.returnPolicy);
+    minimumOrderQuantityController = TextEditingController(
+        text: widget.product.minimumOrderQuantity.toString());
   }
 
   Future<void> updateProductDetails() async {
@@ -82,11 +92,17 @@ class _ProductDetailState extends State<ProductDetail> {
       thumbnail: widget.product.thumbnail,
     );
 
-    List<Product>? updatedProducts = await updateProduct(updatedProduct);
+    Product? updatedResponse = await updateProduct(updatedProduct);
 
-    if (updatedProducts != null) {
+    if (updatedResponse != null) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Product updated successfully!")),
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProductDetailPage(product: updatedResponse),
+        ),
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -97,16 +113,7 @@ class _ProductDetailState extends State<ProductDetail> {
     setState(() {
       isLoading = false;
     });
-    
-    Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(
-          builder: (context) => ProductDetailPage(product: updatedProduct),
-        ),
-      );
   }
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -117,46 +124,103 @@ class _ProductDetailState extends State<ProductDetail> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              CustomTextField(controller: titleController, hintText: "Add a title"),
+              CustomTextField(
+                  controller: titleController, hintText: "Add a title"),
               const SizedBox(height: 5),
-              CustomTextField(controller: descriptionController, hintText: "Write a detailed description", maxLines: 3),
+              CustomTextField(
+                  controller: descriptionController,
+                  hintText: "Write a detailed description",
+                  maxLines: 3),
               const SizedBox(height: 5),
-              CustomTextField(controller: categoryController, hintText: "What is the category"),
+              CustomTextField(
+                  controller: categoryController,
+                  hintText: "What is the category"),
               const SizedBox(height: 5),
               Row(
                 children: [
-                  Expanded(child: CustomTextField(controller: priceController, hintText: "Add price")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: priceController, hintText: "Add price")),
                   const SizedBox(width: 5),
-                  Expanded(child: CustomTextField(controller: discountPercentageController, hintText: "Add discount percentage")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: discountPercentageController,
+                          hintText: "Add discount percentage")),
                 ],
               ),
               const SizedBox(height: 5),
               Row(
                 children: [
-                  Expanded(child: CustomTextField(controller: brandController, hintText: "Add brand")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: brandController, hintText: "Add brand")),
                   const SizedBox(width: 5),
-                  Expanded(child: CustomTextField(controller: stockController, hintText: "Add available stock")),
-                  Expanded(child: CustomTextField(controller: skuController, hintText: "Add SKU")),
-                  Expanded(child: CustomTextField(controller: weightController, hintText: "Add weight")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: stockController,
+                          hintText: "Add available stock")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: skuController, hintText: "Add SKU")),
+                  Expanded(
+                      child: CustomTextField(
+                          controller: weightController,
+                          hintText: "Add weight")),
                 ],
               ),
               const SizedBox(height: 5),
-              CustomTextField(controller: warrantyInformationController, hintText: "Write warranty information"),
+              CustomTextField(
+                  controller: warrantyInformationController,
+                  hintText: "Write warranty information"),
               const SizedBox(height: 5),
-              CustomTextField(controller: shippingInformationController, hintText: "Write shipping information"),
+              CustomTextField(
+                  controller: shippingInformationController,
+                  hintText: "Write shipping information"),
               const SizedBox(height: 5),
-              CustomTextField(controller: availabilityStatusController, hintText: "Write availability status"),
+              CustomTextField(
+                  controller: availabilityStatusController,
+                  hintText: "Write availability status"),
               const SizedBox(height: 5),
-              CustomTextField(controller: returnPolicyController, hintText: "What is the return policy"),
+              CustomTextField(
+                  controller: returnPolicyController,
+                  hintText: "What is the return policy"),
               const SizedBox(height: 5),
-              CustomTextField(controller: minimumOrderQuantityController, hintText: "What is the minimum order quantity"),
+              CustomTextField(
+                  controller: minimumOrderQuantityController,
+                  hintText: "What is the minimum order quantity"),
               const SizedBox(height: 10),
-              CustomButton(
-                onPressed: ()async{
-                  isLoading ? null : updateProductDetails;
-                },
-                text: isLoading ? "Updating..." : "Update",
+              ElevatedButton(
+                onPressed: isLoading
+                    ? null
+                    : () async {
+                        await updateProductDetails();
+                      },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color.fromRGBO(23, 59, 69, 1),
+                  foregroundColor: Colors.white,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 20, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  elevation: 5,
+                ),
+                child: isLoading
+                    ? const SizedBox(
+                        width: 20,
+                        height: 20,
+                        child: CircularProgressIndicator(
+                          strokeWidth: 2,
+                          color: Colors.white,
+                        ),
+                      )
+                    : const Text(
+                        "Update",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.w500),
+                      ),
               ),
+              const SizedBox(height: 30),
             ],
           ),
         ),
